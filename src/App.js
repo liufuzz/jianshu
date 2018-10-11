05daf61c9a70c66dcc687a879488317a45feec2d
liufu <liufu9935@gmail.com>
2018-10-11 22:24:15 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './style';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div>hello</div>
+      </div>
+    );
+  }
+}
+
+export default App;
